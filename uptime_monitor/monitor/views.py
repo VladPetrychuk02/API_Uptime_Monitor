503bde744cd989c5b83c560fb172023dc50ca13d
Vladyslav Petrychuk <vladyslav.petrychuk42@gmail.com>
2025-04-25 12:58:41 +0300

    Add history viewset

--- uptime_monitor/monitor/views.py
+++ uptime_monitor/monitor/views.py
@@ -1,6 +1,6 @@
-from rest_framework import viewsets, permissions
-from .models import MonitoredURL
-from .serializers import MonitoredURLSerializer
+from rest_framework import viewsets, permissions, filters
+from .models import MonitoredURL, UptimeHistory
+from .serializers import MonitoredURLSerializer, UptimeHistorySerializer
 
 class MonitoredURLViewSet(viewsets.ModelViewSet):
     serializer_class = MonitoredURLSerializer
@@ -11,3 +11,15 @@ class MonitoredURLViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+        
+class UptimeHistoryViewSet(viewsets.ReadOnlyModelViewSet):
+    serializer_class = UptimeHistorySerializer
+    permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [filters.OrderingFilter, filters.SearchFilter]
+    ordering_fields = ['checked_at']
+    search_fields = ['status']
+
+    def get_queryset(self):
+        return UptimeHistory.objects.filter(
+            monitored_url__user=self.request.user
+        ).order_by('-checked_at')
