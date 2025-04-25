1274d675fe4d0849ba709251bbd3a011a213cb0b
Vladyslav Petrychuk <vladyslav.petrychuk42@gmail.com>
2025-04-25 12:39:24 +0300

    Config viewsets

--- uptime_monitor/monitor/views.py
+++ uptime_monitor/monitor/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import MonitoredURL
+from .serializers import MonitoredURLSerializer
 
-# Create your views here.
+class MonitoredURLViewSet(viewsets.ModelViewSet):
+    serializer_class = MonitoredURLSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return MonitoredURL.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
