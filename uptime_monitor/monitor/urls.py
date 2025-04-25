7dd04ba3cce76225526af686620e03f92a6a76a5
Vladyslav Petrychuk <vladyslav.petrychuk42@gmail.com>
2025-04-25 12:58:52 +0300

    Add routers for history

--- uptime_monitor/monitor/urls.py
+++ uptime_monitor/monitor/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import MonitoredURLViewSet
+from .views import MonitoredURLViewSet, UptimeHistoryViewSet
 
 router = DefaultRouter()
 router.register(r'urls', MonitoredURLViewSet, basename='monitored-url')
+router.register(r'history', UptimeHistoryViewSet, basename='uptime-history')
 
 urlpatterns = [
     path('', include(router.urls)),
