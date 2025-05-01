b719a2753bd93f3996db3fc0e8cc3c7d48503be4
Vladyslav Petrychuk <vladyslav.petrychuk42@gmail.com>
2025-05-01 18:31:08 +0300

    Create data validation schemas

---
+++ uptime_monitor/monitor/schemas.py
@@ -0,0 +1,34 @@
+from pydantic import BaseModel
+from datetime import datetime
+
+
+class MonitoredURLBase(BaseModel):
+    url: str
+    check_interval: int
+    webhook_url: str
+
+
+class MonitoredURLCreate(MonitoredURLBase):
+    user_id: int
+
+
+class MonitoredURL(MonitoredURLBase):
+    id: int
+    status: str
+    last_checked: datetime | None = None
+
+    model_config = {
+        "from_attributes": True
+    }
+
+
+class UptimeHistory(MonitoredURLBase):
+    id: int
+    url: str
+    monitored_url_id: int
+    status: str
+    checked_at: datetime
+
+    model_config = {
+        "from_attributes": True
+    }
