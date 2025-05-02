8bea7f6630108b450384b4cf77e937a5b12e0a13
Vladyslav Petrychuk <vladyslav.petrychuk42@gmail.com>
2025-05-02 11:41:18 +0300

    Changing tasks.py to call functions from crud.py

--- uptime_monitor/monitor/tasks.py
+++ uptime_monitor/monitor/tasks.py
@@ -3,9 +3,9 @@ import requests
 from datetime import datetime, timedelta
 from celery import shared_task
 
-from uptime_monitor.database import SessionLocal, MonitoredURL, UptimeHistory
+from uptime_monitor.database import SessionLocal, MonitoredURL
 from monitor.utils import send_webhook
-
+from .crud import create_uptime_history, update_monitored_url_status
 
 logger = logging.getLogger(__name__)
 
@@ -20,23 +20,6 @@ class URLStatusChecker:
             return "UP" if response.status_code < 400 else "DOWN"
         except Exception:
             return "DOWN"
-
-    def update_status(self, new_status):
-        self.url_obj.status = new_status
-        self.url_obj.last_checked = self.now
-        db = SessionLocal()
-
-        db.merge(self.url_obj)
-        db.flush() 
-
-        db.add(UptimeHistory(
-            monitored_url_id=self.url_obj.id,
-            status=new_status,
-            checked_at=self.now,
-            url=self.url_obj.url,
-            check_interval=self.url_obj.check_interval
-        ))
-        db.commit()
 
     def send_webhook_alert(self, new_status):
         if self.url_obj.webhook_url:
@@ -58,6 +41,9 @@ def check_url_status():
                     logger.info(f"Status for {url_obj.url} changed to {new_status}")
                     checker.send_webhook_alert(new_status)
 
-                checker.update_status(new_status)
+                    update_monitored_url_status(db, url_obj, new_status)
+
+                    create_uptime_history(db, url_obj.id, new_status, checker.now, url_obj.check_interval, url_obj.url)
+
     finally:
         db.close()
