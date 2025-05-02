a0678214de11a352ca3e6844f7441cb0000bf684
Vladyslav Petrychuk <vladyslav.petrychuk42@gmail.com>
2025-05-02 12:28:01 +0300

    Add periodic task launch after adding URLs

--- uptime_monitor/uptime_monitor/celery.py
+++ uptime_monitor/uptime_monitor/celery.py
@@ -1,7 +1,9 @@
-from monitor import tasks  # noqa: F401
 import os
 from celery import Celery
+from celery.schedules import crontab
 from dotenv import load_dotenv
+
+from monitor import tasks  # noqa: F401
 
 load_dotenv()
 
@@ -12,3 +14,10 @@ app.conf.broker_url = os.getenv("CELERY_BROKER_URL")
 app.conf.result_backend = os.getenv("CELERY_RESULT_BACKEND")
 app.conf.accept_content = ["json"]
 app.conf.task_serializer = "json"
+
+app.conf.beat_schedule = {
+    'check-url-status-every-minute': {
+        'task': 'monitor.tasks.check_url_status',
+        'schedule': crontab(minute='*'),
+    },
+}
