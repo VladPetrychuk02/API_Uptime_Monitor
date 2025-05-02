65233a2aecd49d52d109af177b032a2fb4118850
Vladyslav Petrychuk <vladyslav.petrychuk42@gmail.com>
2025-05-02 11:42:42 +0300

    Import and use funcs from crud.py

--- uptime_monitor/monitor/api.py
+++ uptime_monitor/monitor/api.py
@@ -8,9 +8,10 @@ from uptime_monitor.auth import get_current_user
 from uptime_monitor.database import SessionLocal, User
 from uptime_monitor.database import MonitoredURL as MonitoredURLModel, UptimeHistory as UptimeHistoryModel
 from monitor.schemas import MonitoredURLCreate, MonitoredURL, UptimeHistory as UptimeHistorySchema
+from .crud import create_monitored_url, get_all_monitored_urls, get_uptime_history
+
 
 router = APIRouter()
-
 
 def get_db():
     db = SessionLocal()
@@ -19,47 +20,25 @@ def get_db():
     finally:
         db.close()
 
-
 @router.post("/urls/", response_model=MonitoredURL)
-def create_monitored_url(url: MonitoredURLCreate, db: Session = Depends(get_db)):
-    db_url = MonitoredURLModel(
-        url=url.url,
-        check_interval=url.check_interval,
-        webhook_url=url.webhook_url,
-        user_id=url.user_id
-    )
-    db.add(db_url)
-    db.commit()
-    db.refresh(db_url)
-    return db_url
-
+def create_monitored_url_endpoint(url: MonitoredURLCreate, db: Session = Depends(get_db)):
+    return create_monitored_url(db, url.user_id, url.url, url.check_interval, url.webhook_url)
 
 @router.get("/urls/", response_model=list[MonitoredURL])
 def get_urls(
     db: Session = Depends(get_db),
     current_user: User = Depends(get_current_user)
 ):
-    return db.query(MonitoredURLModel).filter(MonitoredURLModel.user_id == current_user.id).all()
-
+    return get_all_monitored_urls(db)
 
 @router.get("/history/", response_model=list[UptimeHistorySchema])
-def get_uptime_history(
+def get_uptime_history_endpoint(
     db: Session = Depends(get_db),
     current_user: User = Depends(get_current_user),
     from_date: Optional[datetime] = Query(None, alias="from"),
     to_date: Optional[datetime] = Query(None, alias="to")
 ):
-    query = db.query(UptimeHistoryModel).join(MonitoredURLModel, MonitoredURLModel.id == UptimeHistoryModel.monitored_url_id).filter(
-        MonitoredURLModel.user_id == current_user.id
-    )
-
-    if from_date:
-        query = query.filter(UptimeHistoryModel.checked_at >= from_date)
-    if to_date:
-        query = query.filter(UptimeHistoryModel.checked_at <= to_date)
-
-    return query.order_by(UptimeHistoryModel.checked_at.desc()).all()
-
+    return get_uptime_history(db, current_user.id, from_date, to_date)
 
 @router.get("/stats/")
 def get_uptime_status_stats(
